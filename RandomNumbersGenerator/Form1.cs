fd96218d273490065f13eb030c75d2aa855c6be8
Ibtasam Ahmad <85065814+Ibtasam-Ahmad@users.noreply.github.com>
2023-10-19 11:24:39 +0500

    Add files via upload

---
+++ RandomNumbersGenerator/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomNumbersGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random obj = new Random();
+            int num = 100;
+
+            for (int i = 0; i < num; i++)
+            {
+                //int var;
+                //var = obj.Next(1, 6);
+                textBox1.Text = textBox1.Text +"\r\t"+ i + " = " + obj.Next(1, 7);
+                System.Threading.Thread.Sleep(20);
+                textBox1.Refresh();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random obj = new Random();
+            int num = 100;
+
+            for (int i = 0; i < num; i++)
+            {
+                //int var;
+                //var = obj.Next();
+                textBox1.Text = textBox1.Text + "\r\t" + i + " = " + obj.Next();
+                System.Threading.Thread.Sleep(20);
+                textBox1.Refresh();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Random obj = new Random();
+            int num = 100;
+
+            for (int i = 0; i < num; i++)
+            {
+                //int var;
+                //var = obj.Next();
+                textBox1.Text = textBox1.Text + "\r\t" + i + " = " + obj.Next(60);
+                System.Threading.Thread.Sleep(20);
+                textBox1.Refresh();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Random obj = new Random();
+            int num = 100;
+
+            for (int i = 0; i < num; i++)
+            {
+                //int var;
+                //var = obj.Next();
+                textBox1.Text = textBox1.Text + "\r\t" + i + " = " + obj.NextDouble();
+                System.Threading.Thread.Sleep(20);
+                textBox1.Refresh();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Random obj = new Random();
+            int num = 100;
+            double min = 0.1, max = 1.1;
+
+            for (int i = 0; i < num; i++)
+            {
+                //int var;
+                //var = obj.Next();
+                textBox1.Text = textBox1.Text + "\r\t" + i + " = " + ( min + (max - min) * obj.NextDouble());
+                System.Threading.Thread.Sleep(20);
+                textBox1.Refresh();
+            }
+        }
+    }
+}
